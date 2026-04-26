45bfec03c5f52efdea8ef7d6d778d8d4f4577679
Emilio Castro <sleepdreaming@gmail.com>
2026-04-25 18:37:33 -0600

    Arreglando errores de terraform

--- terraform/computo.tf
+++ terraform/computo.tf
@@ -24,6 +24,10 @@ resource "aws_instance" "frontend" {
   associate_public_ip_address = true
   key_name                    = aws_key_pair.bootcamp.key_name
   vpc_security_group_ids      = [aws_security_group.taller_iac_bootcamperu_publico.id, aws_security_group.taller_iac_bootcamperu_comun.id]
+  metadata_options = {
+    http_endpoint = "enabled"
+    http_tokens = "required"
+  }
 
   tags = {
     Name = "Instancia Frontend"
@@ -40,6 +44,10 @@ resource "aws_instance" "backend" {
   associate_public_ip_address = false
   key_name                    = aws_key_pair.bootcamp.key_name
   vpc_security_group_ids      = [aws_security_group.taller_iac_bootcamperu_privado.id, aws_security_group.taller_iac_bootcamperu_comun.id]
+  metadata_options = {
+    http_endpoint = "enabled"
+    http_tokens = "required"
+  }
 
   tags = {
     Name = "Instancia Backend"
